31dbab330b1f9479a1d50f60373bcc4b97ba6f18
Roman Páleník <brzdatv@gmail.com>
2020-07-09 23:58:48 +0200

    Pridane dalsie kriteria, rozrobneny datum

--- kody.py
+++ kody.py
@@ -1,6 +1,11 @@
 #zoznam musí byť tvare Meno	Priezvisko	Dátum narodenia	Ulica
 
 
+def dateConverter(arr):
+    for info in arr:
+        print(info[2])
+
+
 def creatingCode(arr, criteria): #sortne to podla mena cez bubble sort
     n = len(arr)
 
@@ -33,11 +38,28 @@ def creatingCodeThirdLetter(arr, criteria): #sortne to podla mena cez bubble sor
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
-with open('zoznam.txt', 'r', encoding='utf8') as file:
+def creatingCodeLength(arr, criteria): #sortne to podla mena cez bubble sort
+    n = len(arr)
+
+    # Traverse through all array elements
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if len(arr[j][criteria]) > len(arr[j + 1][criteria]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+with open('zoznam.txt', 'r', encoding='utf8') as mainFile:
     arrayWithInfo = []
-    for line in file:
+    for line in mainFile:
         arrayWithInfo.append(line.split())
 
+    dateConverter(arrayWithInfo)
     #sortovanie podľa mena
     creatingCode(arrayWithInfo, 0)
 
@@ -70,3 +92,19 @@ with open('zoznam.txt', 'r', encoding='utf8') as file:
         for info in arrayWithInfo:
             file5.write(info[0] + " ")
             file5.write(info[1] + "\n")
+
+    #sortovanie podľa mena dĺžka
+    creatingCodeLength(arrayWithInfo, 0)
+
+    with open("podlaMenaDlzka.txt", "a", encoding='utf8') as file6:
+        for info in arrayWithInfo:
+            file6.write(info[0] + " ")
+            file6.write(info[1] + "\n")
+
+    #sortovanie podľa priezviska dĺžka
+    creatingCodeLength(arrayWithInfo, 1)
+
+    with open("podlaPriezviskaDlzka.txt", "a", encoding='utf8') as file7:
+        for info in arrayWithInfo:
+            file7.write(info[0] + " ")
+            file7.write(info[1] + "\n")
